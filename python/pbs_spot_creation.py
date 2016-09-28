514e695a4d10a4bf375c47aadfee8ba3e1ed46ab
anegronrivera <alfredonegronrivera@gmail.com>
2016-09-28 10:56:44 -0400

    Updated non-sobp scripts

--- python/pbs_spot_creation.py
+++ python/pbs_spot_creation.py
@@ -22,19 +22,19 @@ def read_file(file_name):
 machine = read_file('../Machine Setup Parser/machine_setups/pbs_machine.json')
 
 # ------ Create the PBS Spot Lists per Energy ------
-start_energy = 120
-delta_energy = 3
+start_energy = 120.
+delta_energy = 3.
 
 # Add a spot along CAX and at corners of a 3x3 box to each energy
 spots_by_energy = []
 for i in range(0,3):
 	e = start_energy + i * delta_energy
 	spots_by_energy.append([])
-	spots_by_energy[i].append(dosimetry.make_spot([0,0], 0.5, e))
-	spots_by_energy[i].append(dosimetry.make_spot([-3,-3], 0.5, e))
-	spots_by_energy[i].append(dosimetry.make_spot([-3, 3], 0.5, e))
-	spots_by_energy[i].append(dosimetry.make_spot([ 3, 3], 0.5, e))
-	spots_by_energy[i].append(dosimetry.make_spot([ 3,-3], 0.5, e))
+	spots_by_energy[i].append(dosimetry.make_spot([0.,0.], 0.5, e))
+	spots_by_energy[i].append(dosimetry.make_spot([-3.,-3.], 0.5, e))
+	spots_by_energy[i].append(dosimetry.make_spot([-3., 3.], 0.5, e))
+	spots_by_energy[i].append(dosimetry.make_spot([ 3., 3.], 0.5, e))
+	spots_by_energy[i].append(dosimetry.make_spot([ 3.,-3.], 0.5, e))
 
 
 # Create Plan will read an existing DICOM Plan file (the first beam will be 
@@ -50,8 +50,7 @@ study_id = dicom.make_rt_study_from_dir(iam, plan_file_path)
 
 # Generate the new plan
 res = dosimetry.create_plan(iam, study_id, machine, spots_by_energy)
-
-plan_data = json.loads(thinknode.get_immutable(iam, "rt_types", res))
+plan_data = thinknode.get_immutable(iam, "dosimetry", res, False)
 
 stream = open(output_file, 'wb')
 stream.write(base64.b64decode(plan_data["blob"]))
